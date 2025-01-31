a6789f795bebf48e927fe798835029206856df63
aleks <ayergoo@gmail.com>
2025-01-31 18:33:30 +0100

    added side nav bar and split main page into grid

--- DndReexam/Program.cs
+++ DndReexam/Program.cs
@@ -1,10 +1,14 @@
 using DndReexam.Components;
+using Havit.Blazor.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHxServices();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
